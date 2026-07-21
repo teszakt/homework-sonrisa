48c0e08ab42e7d4ee651c39ad1a38879a529871a
teszak.tamas <teszak.tamas@gmail.com>
2026-07-21 16:58:30 +0200

    add custom exception

---
+++ alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/exception/AlertException.java
@@ -0,0 +1,9 @@
+package hu.teszakt.homework.sonrisa.service.exception;
+
+public class AlertException extends RuntimeException{
+
+    public AlertException(String message) {
+        super(message);
+    }
+
+}
