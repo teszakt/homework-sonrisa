c388862a012aea12b7992c64d5ae1b148ffed95c
teszak.tamas <teszak.tamas@gmail.com>
2026-07-21 11:56:50 +0200

    service conf

--- alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/configuration/AlertServiceConfiguration.java
+++ alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/configuration/AlertServiceConfiguration.java
@@ -1,12 +1,20 @@
 package hu.teszakt.homework.sonrisa.service.configuration;
 
 import hu.teszakt.homework.sonrisa.db.configuration.AlertDbConfiguration;
+import hu.teszakt.homework.sonrisa.service.impl.AlertServiceImpl;
+import hu.teszakt.homework.sonrisa.service.impl.EventServiceImpl;
+import hu.teszakt.homework.sonrisa.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(
-        basePackageClasses = AlertDbConfiguration.class
+        basePackageClasses = {
+                AlertDbConfiguration.class,
+                UserServiceImpl.class,
+                AlertServiceImpl.class,
+                EventServiceImpl.class
+        }
 )
 public class AlertServiceConfiguration {
 }
