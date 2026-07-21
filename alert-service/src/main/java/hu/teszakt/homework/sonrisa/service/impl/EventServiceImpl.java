7a55bef07ed10cb8f1e2b0120f2c4bfce9def341
teszak.tamas <teszak.tamas@gmail.com>
2026-07-21 18:14:24 +0200

    add EventMapper

--- alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/impl/EventServiceImpl.java
+++ alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/impl/EventServiceImpl.java
@@ -10,6 +10,7 @@ import hu.teszakt.homework.sonrisa.service.AlertEvaluatorService;
 import hu.teszakt.homework.sonrisa.service.EventService;
 import hu.teszakt.homework.sonrisa.service.NotificationService;
 import hu.teszakt.homework.sonrisa.service.exception.AlertException;
+import hu.teszakt.homework.sonrisa.service.mapper.EventMapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -23,6 +24,7 @@ import java.util.List;
 public class EventServiceImpl extends AbstractBaseService implements EventService {
 
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
     private final AlertEvaluatorService alertEvaluatorService;
     private final NotificationService notificationService;
 
@@ -30,8 +32,7 @@ public class EventServiceImpl extends AbstractBaseService implements EventServic
     public EventCreationResponse createEvent(@NonNull EventCreationRequest request) {
         log.debug("Running createEvent service method for event: {}", request.getEvent().getAlertType());
         try {
-            // TODO mapstruct mapping
-            final EventEntity eventEntity = null;
+            final EventEntity eventEntity = eventMapper.mapToEventEntity(request);
             eventRepository.save(eventEntity);
 
             final List<AlertEntity> matchingAlerts = alertEvaluatorService.findMatchingAlerts(request.getEvent());
---
+++ alert-service/src/main/java/hu/teszakt/homework/sonrisa/service/mapper/EventMapper.java
@@ -0,0 +1,21 @@
+package hu.teszakt.homework.sonrisa.service.mapper;
+
+import hu.teszakt.homework.sonrisa.db.entity.EventEntity;
+import hu.teszakt.homework.sonrisa.model.enums.EventCreationRequest;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(
+        componentModel = "spring",
+        config = MapperBaseConfiguration.class
+)
+public interface EventMapper {
+
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "alertType", source = "event.alertType")
+    @Mapping(target = "createdAt", source = "event.createdAt")
+    @Mapping(target = "message", source = "event.message")
+    EventEntity mapToEventEntity(EventCreationRequest request);
+
+}
